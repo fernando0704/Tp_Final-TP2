05a41b7dd0b883458e511faed1dad955b995afeb
fernando0704 <quiroz.fer@gmail.com>
2022-05-16 21:21:48 -0300

    html

--- index.js
+++ index.js
@@ -13,9 +13,11 @@ const processDelete = require('./processors/processDelete.js')
 
 //server.listen(5000)
 
-const app = express()
+const app = express() //primero obtenemos un objeto llamado "app"
 
-app.get('/', (request,response)=>{
+app.use('/', express.static('html')) //devuelve una ruta y manejadores de esas rutas
+
+app.get('/', (request,response, next)=>{
     response.writeHead(200, {'Content-Type': 'application/json'})
     const body = {mensaje: 'Hola Mundo 2'}
     response.end(JSON.stringify(body))
@@ -27,6 +29,13 @@ app.post('/',(request, response)=>{
 })
 
 app.delete('/', processDelete)
+
+//............... para el manejo de productos:
+
+app.post('/productos', creadorProductos)
+app.delete('/productos', borradorProductos)
+app.get('/productos', listadorProductos)
+//................
 
 app.listen(5000,()=>{
     console.log('Ejecutando servidor de pruebas');
